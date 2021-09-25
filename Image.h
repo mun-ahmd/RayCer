6875902aae0c815de376e358be908f7ba64c6c5c
mun-ahmd <muneeb0ahmed3@gmail.com>
2021-09-25 11:42:15 +0530

    save to bitmap added

--- Image.h
+++ Image.h
@@ -2,9 +2,10 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <functional>
-
+#include <algorithm>
 #include <assert.h>
 
 #include "Vec.h"
@@ -45,9 +46,38 @@ public:
 			if (line_string[0] == 'P' && line_string[1] == '3')
 				metadata_loaded = true;
 		}
+
+		metadata_loaded = false;
 		unsigned int width, height;
+		while (!metadata_loaded)
+		{
+			std::getline(file, line_string);
+			if (line_string[0] == '#')
+				continue;
+			else
+			{
+				std::stringstream w_and_h(line_string);
+				w_and_h >> width >> height;
+				metadata_loaded = true;
+			}
+		}
+
+		metadata_loaded = false;
 		unsigned int max_val;
-		file >> width >> height >> max_val;
+		while (!metadata_loaded)
+		{
+			std::getline(file, line_string);
+			if (line_string[0] == '#')
+				continue;
+			else
+			{
+				std::stringstream max_val_ss(line_string);
+				max_val_ss >> max_val;
+				metadata_loaded = true;
+			}
+		}
+
+
 		float max_val_f = (float)max_val;
 
 		__GeneralImage__<PixelType> img(width, height);
@@ -87,6 +117,106 @@ public:
 			file << std::endl;
 		}
 		file.close();
+	}
+
+	void store_bmp(std::string filedir) const
+	{
+		std::ofstream file;
+		file.open(filedir);
+		assert(file.is_open(), ("unable to open file: " + filedir).c_str());		
+		struct
+		{
+			//header
+			char signature1 = 'B';
+			char signature2 = 'M';
+			unsigned int file_size;
+			unsigned int unused_val = 0;
+			unsigned int data_offset = 54;
+
+			//info header
+			unsigned int info_header_size = 40;
+			int width;
+			int height;
+			unsigned short planes = 1;
+			unsigned short bits_per_pixel = 24;
+			unsigned int compression = 0;
+			unsigned int image_data_size;
+			int X_pixels_per_meter = 1000;
+			int Y_pixels_per_meter = 1000;
+			unsigned int num_colors_used_actual = 256*256*256;
+			unsigned int imp_colors = 0;
+
+			void write_header(std::ofstream& file, std::function<void(void*,unsigned int)> lew)
+			{
+				file.write(&signature1, 1);
+				file.write(&signature2, 1);
+				lew(&file_size,sizeof(int));
+				lew(&unused_val,sizeof(int));
+				lew(&data_offset,sizeof(int));
+				lew(&info_header_size,sizeof(int));
+				lew(&width,sizeof(int));
+				lew(&height, sizeof(int));
+				lew(&planes,sizeof(short));
+				lew(&bits_per_pixel,sizeof(short));
+				lew(&compression,sizeof(int));
+				lew(&image_data_size,sizeof(int));
+				lew(&X_pixels_per_meter,sizeof(int));
+				lew(&Y_pixels_per_meter,sizeof(int));
+				lew(&num_colors_used_actual,sizeof(int));
+				lew(&imp_colors,sizeof(int));
+			}
+		} bmp_h;
+
+		bmp_h.width = width;
+		bmp_h.height = height;
+		unsigned int padding_size = (width * 3) % 4;
+		bmp_h.image_data_size = width * height * (bmp_h.bits_per_pixel / 8) + height*padding_size;
+		
+		bmp_h.file_size = bmp_h.data_offset + bmp_h.image_data_size;
+
+		auto dano = bmp_h.image_data_size;
+		bmp_h.image_data_size = 0;
+		bmp_h.write_header(file, [&file](void* data, unsigned int data_size)
+			{
+				uint32_t endiannes_check_int = 0x1;
+				if (((char*)&endiannes_check_int)[0] == 1)
+				{
+					//system is little endian
+					file.write((char*)data, data_size);
+				}
+				else
+				{
+					//system is big endian
+					char* bytes = (char*)&data;
+					std::reverse(bytes, bytes + data_size);
+					file.write(bytes, data_size);
+				}
+			});
+		bmp_h.image_data_size = dano;
+		
+		unsigned char* _image_data = new unsigned char[bmp_h.image_data_size];
+		unsigned int i_data_i = 0;
+		for (int y = (this->height - 1); y > -1; y -= 1)
+		{
+			for (unsigned int x = 0; x < width; ++x)
+			{
+				unsigned char rgb[3] = { 0,0,0 };
+				at(x, y) << rgb;
+				//bitmap stores as bgr
+				_image_data[i_data_i] = rgb[2];
+				_image_data[i_data_i + 1] = rgb[1];
+				_image_data[i_data_i + 2] = rgb[0];
+				i_data_i += 3;
+			}
+
+			for (unsigned int i = 0; i < padding_size;++i)
+			{
+				_image_data[i_data_i] = 0;	//to make divisible by 4
+				i_data_i++;
+			}
+		}
+
+		file.write((char*)_image_data, bmp_h.image_data_size);	//todo check if you need static cast
 	}
 
 	__GeneralImage__(unsigned int width, unsigned int height) : width(width), height(height)
@@ -153,9 +283,9 @@ public:
 	}
 	void operator>>(const unsigned char(&rgb)[3])
 	{
-		x() = static_cast<double>(rgb[0])/255.99;
-		y() = static_cast<double>(rgb[1])/255.99;
-		z() = static_cast<double>(rgb[2])/255.99;
+		x() = static_cast<double>(rgb[0])/255.0;
+		y() = static_cast<double>(rgb[1])/255.0;
+		z() = static_cast<double>(rgb[2])/255.0;
 	}
 };
 
--- RayCer.cpp
+++ RayCer.cpp
@@ -14,11 +14,11 @@ struct xzc
 int main()
 {
 
-	Image i = Image::load_ppm("C:\\Users\\munee\\Desktop\\Media Encoding Projects\\ppm\\ppmTest.ppm");
+	Image i = Image::load_ppm("C:\\Users\\munee\\Desktop\\Media Encoding Projects\\Jett.ppm");
+	//Image i = Image::load_ppm("test.ppm");
 	vec3 v({2.f,3.0,0});
 
-	
-
-	i.store_ppm("test.ppm");
+	i.store_ppm("Jett2.ppm");
+	i.store_bmp("Jett.bmp");
 	return 0;
 }
